d9030c7be040a8d23d0abc7f49f493c9e940222b
mrninhvn <1.ninh.vn@gmail.com>
2020-02-27 01:08:50 +0700

    grid 2

--- Mbit_IR.ts
+++ Mbit_IR.ts
@@ -24,11 +24,11 @@ namespace UMI_Remote {
     * button pushed.
     */
     //% blockId=Mbit_IR_received_event
-    //% blockGap=20 weight=89
     //% block="on |%btn| button pressed"
     //% button.fieldEditor="gridpicker"
     //% button.fieldOptions.columns=3
     //% button.fieldOptions.tooltips="false"
+    //% weight=69
     //% shim=Mbit_IR::onPressEvent
     export function onPressEvent(btn: RemoteButton, body:Action): void {
       return
